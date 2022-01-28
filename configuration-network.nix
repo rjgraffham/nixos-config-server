30e55372946d59b7cd49486f3ad9ad227b45b03e
RJ Graffham <psquid@psquid.net>
2022-01-28 19:07:14 +0000

    Set nameservers to Cloudflare with fallback to Google

--- configuration-network.nix
+++ configuration-network.nix
@@ -8,6 +8,8 @@
   };
   networking.wireless.environmentFile = config.age.secrets.wireless.path;
 
+  networking.nameservers = [ "1.1.1.1" "8.8.8.8" ];
+
   services.openssh = {
     enable = true;
     permitRootLogin = "no";

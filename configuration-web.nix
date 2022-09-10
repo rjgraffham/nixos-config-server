9fb87ce9b8ec23fa01ae1fe574061d0874b43f7f
RJ Graffham <psquid@psquid.net>
2022-09-10 23:31:43 +0000

    Update freshrss version

--- configuration-web.nix
+++ configuration-web.nix
@@ -74,7 +74,7 @@
           # (could still fail if the tag is ever deleted)
           # this prevents getting updates automatically, but so would caching otherwise,
           # and following the release feed to manually bump the version here is no big deal
-          image = "freshrss/freshrss:1.19.2-arm";
+          image = "freshrss/freshrss:1.20.0-arm";
           volumes = [
             "freshrss-data:/var/www/FreshRSS/data"
             "${freshrssExtensions}:/var/www/FreshRSS/extensions"

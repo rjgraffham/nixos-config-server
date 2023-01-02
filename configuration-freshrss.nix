a954415f5d42a0537e1c89469ee33342d40f5f16
RJ Graffham <psquid@psquid.net>
2023-01-02 02:30:11 +0000

    Add YouTubeChannel2RssFeed freshrss extension

--- configuration-freshrss.nix
+++ configuration-freshrss.nix
@@ -8,8 +8,12 @@
 
   services.freshrss = let 
    frssLangfeld = builtins.fetchTarball {
-      url = "https://github.com/langfeld/FreshRSS-extensions/archive/refs/heads/master.tar.gz";
+      url = "https://github.com/langfeld/FreshRSS-extensions/archive/49c33360638b4ed1baf8217d862e1f05e01812c4.tar.gz";
       sha256 = "1mbxmbb8bszgm8hxxn3vm5k01rg61nldi1i81pq09pv2zpvnz5pi";
+    };
+   frssCntools = builtins.fetchTarball {
+      url = "https://github.com/cn-tools/cntools_FreshRssExtensions/archive/911585c8e0607308dc7d76041fa5a256fffcfc57.tar.gz";
+      sha256 = "1n6y4plndmkz07m27hanhiir20g57xlxpm80y1raxdw2xiyksilh";
     };
   in {
     enable = true;
@@ -18,6 +22,7 @@
         mkdir -p $out/extensions
         ln -s ${frssLangfeld}/xExtension-FixedNavMenu $out/extensions/xExtension-FixedNavMenu
         ln -s ${frssLangfeld}/xExtension-TouchControl $out/extensions/xExtension-TouchControl
+        ln -s ${frssCntools}/xExtension-YouTubeChannel2RssFeed $out/extensions/xExtension-YouTubeChannel2RssFeed
       '';
     });
     database.type = "sqlite";

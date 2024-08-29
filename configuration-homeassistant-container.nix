c4ddea26cf6a69d38d87bcef8e16ef10e7d8e9f9
RJ Graffham <psquid@psquid.net>
2024-08-29 18:02:05 +0000

    (nixos/rpi) Expose dbus to home-assistant container

--- configuration-homeassistant-container.nix
+++ configuration-homeassistant-container.nix
@@ -6,6 +6,7 @@
   virtualisation.oci-containers.containers.homeassistant = {
     volumes = [
       "home-assistant:/config"
+      "/var/run/dbus:/run/dbus:ro"
     ];
     environment.TZ = "Europe/London";
     image = "ghcr.io/home-assistant/home-assistant:stable";

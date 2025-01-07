7623261994b372d79eab8b6c868384e838af95b7
RJ Graffham <psquid@psquid.net>
2025-01-07 21:00:18 +0000

    (repo) Fix missing ; in sources.nix.

--- sources.nix
+++ sources.nix
@@ -1,6 +1,6 @@
 let
 
-  fetchSource = (src-name: src: builtins.fetchTree { type = "git"; inherit (src) url rev narHash; })
+  fetchSource = (src-name: src: builtins.fetchTree { type = "git"; inherit (src) url rev narHash; });
 
   sourcesJson = builtins.fromJSON (builtins.readFile ./sources.json);
 

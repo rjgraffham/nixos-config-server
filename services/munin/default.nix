260a971faa1a0e76bc62742d21e7219e1b36d43f
RJ Graffham <psquid@psquid.net>
2025-01-08 21:22:27 +0000

    (services/munin) Add plugins for reading kernel PSI metrics.

--- services/munin/default.nix
+++ services/munin/default.nix
@@ -79,6 +79,60 @@
       du -bs /nix/store | cut -f1
     '';
 
+    extraPlugins.psi_some = pkgs.writeScript "psi_some"
+    ''
+      case $1 in
+        config)
+          echo 'graph_args -l 0'
+          echo 'graph_scale no'
+          echo 'graph_title Pressure Stall Information (some)'
+          echo 'graph_vlabel % of last 5 min'
+          echo 'psi_cpu.label cpu'
+          echo 'psi_mem.label memory'
+          echo 'psi_io.label i/o'
+          echo 'graph_category system'
+          echo 'graph_info The percentage of time at least one process was waiting on resources in the last 5 minutes.'
+          exit 0
+          ;;
+      esac
+
+      PATH="${pkgs.gnugrep}/bin:${pkgs.gnused}/bin:$PATH"
+
+      echo -n 'psi_cpu.value '
+      grep '^some' /proc/pressure/cpu | sed -E 's/^.*\savg300=(.*)\s.*$/\1/'
+      echo -n 'psi_mem.value '
+      grep '^some' /proc/pressure/memory | sed -E 's/^.*\savg300=(.*)\s.*$/\1/'
+      echo -n 'psi_io.value '
+      grep '^some' /proc/pressure/io | sed -E 's/^.*\savg300=(.*)\s.*$/\1/'
+    '';
+
+    extraPlugins.psi_full = pkgs.writeScript "psi_full"
+    ''
+      case $1 in
+        config)
+          echo 'graph_args -l 0'
+          echo 'graph_scale no'
+          echo 'graph_title Pressure Stall Information (full)'
+          echo 'graph_vlabel % of last 5 min'
+          echo 'psi_cpu.label cpu'
+          echo 'psi_mem.label memory'
+          echo 'psi_io.label i/o'
+          echo 'graph_category system'
+          echo 'graph_info The percentage of time processes were unable to work due to resource pressure in the last 5 minutes.'
+          exit 0
+          ;;
+      esac
+
+      PATH="${pkgs.gnugrep}/bin:${pkgs.gnused}/bin:$PATH"
+
+      echo -n 'psi_cpu.value '
+      grep '^full' /proc/pressure/cpu | sed -E 's/^.*\savg300=(.*)\s.*$/\1/'
+      echo -n 'psi_mem.value '
+      grep '^full' /proc/pressure/memory | sed -E 's/^.*\savg300=(.*)\s.*$/\1/'
+      echo -n 'psi_io.value '
+      grep '^full' /proc/pressure/io | sed -E 's/^.*\savg300=(.*)\s.*$/\1/'
+    '';
+
     extraPlugins.munin_graph_count = pkgs.writeScript "munin_graph_count"
     ''
       case $1 in

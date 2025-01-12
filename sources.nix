76b512fe8c2eb79c1e4831ec6a0fcbc4d0ea6eff
RJ Graffham <psquid@psquid.net>
2025-01-12 16:49:53 +0000

    (sources) Pass `ref` into fetchTree.

--- sources.nix
+++ sources.nix
@@ -1,6 +1,10 @@
 let
 
-  fetchSource = (src-name: src: builtins.fetchTree { type = "git"; inherit (src) url rev narHash; });
+  fetchSource = (src-name: src: builtins.fetchTree {
+    type = "git";
+    ref = "refs/heads/${src.branch}";
+    inherit (src) url rev narHash;
+  });
 
   sourcesJson = builtins.fromJSON (builtins.readFile ./sources.json);
 

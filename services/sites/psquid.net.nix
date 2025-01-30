5f3a2057ed34c87417e3b68f93ee00ee8e2988e6
RJ Graffham <psquid@psquid.net>
2025-01-30 10:27:49 +0000

    (services/sites/psquid.net) Add blog build.

--- services/sites/psquid.net.nix
+++ services/sites/psquid.net.nix
@@ -1,3 +1,5 @@
+{ pkgs, lib, sources, ... }:
+
 {
   services.nginx.simpleVhosts = {
     "dl.psquid.net" = {
@@ -10,9 +12,24 @@
       webroot = "/var/www/dl-public.psquid.net/html";
     };
 
-    "psquid.net" = {
+    "psquid.net" = let
+      blog = pkgs.stdenv.mkDerivation {
+        name = "blog";
+        src = sources.blog.outPath;
+
+        buildPhase = ''
+          mkdir -p ./built ./src
+          cp -r $src/* ./src/
+
+          ${lib.getExe pkgs.jekyll} build -s ./src -d ./built
+
+          mkdir -p $out
+          cp -r ./built/* $out/
+        '';
+      };
+    in {
       vhostType = "static";
-      webroot = "/var/www/www.psquid.net/html";
+      webroot = "${blog}";
       aliases = [ "www.psquid.net" ];
     };
   };

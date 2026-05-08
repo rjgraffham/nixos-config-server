5476ca97fe961b94dcf1d4101fc313789e85f72e
RJ Graffham <psquid@psquid.net>
2026-05-08 11:43:58 +0000

    (modules/podman) Work around https://github.com/containers/podman/issues/24796

--- modules/podman.nix
+++ modules/podman.nix
@@ -4,6 +4,17 @@
 
   virtualisation.podman.enable = true;
 
+  # work around issue where podman user services are heavily delayed waiting for network-online
+  # (see https://github.com/containers/podman/issues/24796) by creating a dummy unit that forces
+  # the target to be activated
+  systemd.services."dummy-network-online" = {
+    description = "Dummy service to activate network-online.target";
+    after = [ "network-online.target" ];
+    wants = [ "network-online.target" ];
+    script = "echo true";
+    wantedBy = [ "multi-user.target" ];
+  };
+
   virtualisation.podman.autoPrune = {
     enable = true;
     dates = "weekly";
